4179fbc9b4be192a309f73f8c68e1c6350a93251
dhalcyon <dhalcyon@users.noreply.github.com>
2024-01-27 17:59:50 +0700

    add colors and modify main tour page

--- lib/home/main_tour_page.dart
+++ lib/home/main_tour_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiket_wisata/utils/colors.dart';
 
 class MainTourPage extends StatefulWidget {
   const MainTourPage({Key? key}) : super(key: key);
@@ -10,23 +11,38 @@ class MainTourPage extends StatefulWidget {
 class _MainTourPageState extends State<MainTourPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Container(
-      child: Row(
-        children: [
-          Column(
-            children: [],
-          ),
-          Container(
-            width: 45,
-            height: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.blue,
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          child: Container(
+            margin: EdgeInsets.only(top: 45, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text("Negara"),
+                    Text("Kota"),
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: TheColors.mainColor,
+                    ),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+      ],
     ));
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import "dart:ui";
+
+class TheColors {
+  static final Color mainColor = const Color(0xffA7D397);
+  static final Color textColor = const Color(0xffC2C2C2);
+  static final Color icon1Color = const Color(0xffF3B664);
+  static final Color icon2Color = const Color(0xffEC8F5E);
+  static final Color forColor = const Color(0xff555843);
+  static final Color buttonBgColor = const Color(0xffE1F0DA);
+  static final Color signColor = const Color(0xffA9A29F);
+  static final Color titleColor = const Color(0xff50623A);
+  static final Color blackColor = const Color(0xff090807);
+  static final Color yellowColor = const Color(0xffF1EB90);
+}
