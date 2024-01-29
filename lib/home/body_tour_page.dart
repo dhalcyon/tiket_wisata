e0c967a33d3522f350eb54d6699dcadfc0bf3fda
dhalcyon <dhalcyon@users.noreply.github.com>
2024-01-29 12:36:54 +0700

    added matrix in body tour page

--- lib/home/body_tour_page.dart
+++ lib/home/body_tour_page.dart
@@ -12,7 +12,26 @@ class BodyTourPage extends StatefulWidget {
 }
 
 class _BodyTourPageState extends State<BodyTourPage> {
+
   PageController pageController = PageController(viewportFraction: 0.9);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,82 +47,110 @@ class _BodyTourPageState extends State<BodyTourPage> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 200,
-          margin: EdgeInsets.only(left: 5, right: 5),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven ? TheColors.basicColor : TheColors.greyColor,
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/images/wisata1.jpg"),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(left: 40, right: 40, bottom: 10),
+
+    Matrix4 matrix = new Matrix4.identity();
+
+    if (index == _currPageValue.floor()) {
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    } 
+    else if (index == _currPageValue.floor() + 1) {
+      var currScale = _scaleFactor + (_currPageValue - index + 1)*(1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;  // 1/10*220=22
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index)*(1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height * (1 - _scaleFactor)/2 , 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 200,
+            margin: EdgeInsets.only(left: 5, right: 5),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  HighText(text: "Wisata Sunda"),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(
-                          5,
-                          (index) => Icon(
-                            Icons.star,
-                            color: TheColors.basicColor,
-                            size: 15,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      MediumText(text: "4,7"),
-                      SizedBox(width: 10),
-                      MediumText(text: "99"),
-                      SizedBox(width: 10),
-                      MediumText(text: "comments"),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      TextAndIcon(
-                        icon: Icons.circle_sharp,
-                        text: "Norm",
-                        iconColor: TheColors.icon1Color,
-                      ),
-                      TextAndIcon(
-                        icon: Icons.location_on,
-                        text: "1.4km",
-                        iconColor: TheColors.icon1Color,
-                      ),
-                      TextAndIcon(
-                        icon: Icons.access_time_rounded,
-                        text: "20 menit",
-                        iconColor: TheColors.icon1Color,
-                      ),
-                    ],
-                  ),
-                ],
+              color: index.isEven ? TheColors.basicColor : TheColors.greyColor,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/wisata1.jpg"),
               ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 40, right: 40, bottom: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    HighText(text: "Wisata Sunda"),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                            5,
+                            (index) => Icon(
+                              Icons.star,
+                              color: TheColors.basicColor,
+                              size: 15,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        MediumText(text: "4,7"),
+                        SizedBox(width: 10),
+                        MediumText(text: "99"),
+                        SizedBox(width: 10),
+                        MediumText(text: "comments"),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        TextAndIcon(
+                          icon: Icons.circle_sharp,
+                          text: "Norm",
+                          iconColor: TheColors.icon1Color,
+                        ),
+                        TextAndIcon(
+                          icon: Icons.location_on,
+                          text: "1.4km",
+                          iconColor: TheColors.icon1Color,
+                        ),
+                        TextAndIcon(
+                          icon: Icons.access_time_rounded,
+                          text: "20 menit",
+                          iconColor: TheColors.icon1Color,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
